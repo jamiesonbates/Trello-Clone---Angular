e2ae2b910d1a845f6c272e480dd7be67c234c40e
Jamieson Bates <jamiesonbates@gmail.com>
2017-05-03 11:21:13 -0700

    Abstract pieces of home state into various components

---
+++ js/components/list.component.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .component('list', {
+      templateUrl: '/js/components/list.template.html'
+    });
+})();
---
+++ js/components/new-list.component.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .component('newList', {
+      templateUrl: '/js/components/new-list.template.html'
+    })
+})();
